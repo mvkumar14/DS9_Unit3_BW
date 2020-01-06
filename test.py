e3964f551ba56fdd64e4988fda2efc37fed2d6cd
Kumar Veeravel <mvkumar14@gmail.com>
2020-01-06 14:38:45 -0800

    Updated with data and basic sql connection

--- test.py
+++ test.py
@@ -1,35 +1,56 @@
 from flask import Flask, request
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/song', methods = ['POST'])
-def song():
-    """Route for recommendations based on song selected."""
+DB = SQLAlchemy()
 
-    #input
-    song = request.get_json(force=True)
+df = pd.read_csv('SpotifyAudioFeaturesApril2019.csv')
 
-    #model
-    model = "some pickled model"
+conn = sqlite3.connect('songs_df.sqlite3')
 
-    #parameters
-    danceability = song['danceability']
-    energy = song['energy']
+df.to_sql('songs', conn, if_exists='replace')
 
-    #output
-    recommendations = model.predict("parameters")
+# class Song(DB.Model):
+#     """ Model for song entry in database """
+#     id = DB.Column(DB.STring(30))
 
-    return
-
-
-@app.route('/mood')
-def mood():
-    """Route foor recommendations based on mood selected."""
-
-    mood = request.get_json(force=True)
-
-    recommendations =
-
-
-if __name__ == "__main__":
-    app.run()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs_df.sqlite3'
+#
+# @app.route('/song', methods = ['POST'])
+# def song():
+#     """Route for recommendations based on song selected."""
+#
+#     #input
+#     song_id = request.get_json(force=True)
+#
+#     #get parameters:
+#     # use song_id
+#     # songs_df = SELECT * from songs WHERE df_id == song_id
+#     danceability = songs_df['danceability']
+#     energy = songs_df['energy']
+#
+#
+#     #model
+#     model = "some pickled model"
+#
+#     #output
+#     #should be 30 reccomendations
+#     recommendations = model.predict("parameters")
+#
+#     return recommendations
+#
+#
+# @app.route('/mood')
+# def mood():
+#     """Route foor recommendations based on mood selected."""
+#
+#     mood = request.get_json(force=True)
+#
+#     recommendations =
+#
+#
+# if __name__ == "__main__":
+#     app.run()
