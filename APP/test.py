5d2e7a936732ce201f26cf250ea5ed1fc84f28b4
Kumar Veeravel <mvkumar14@gmail.com>
2020-01-06 15:00:49 -0800

    New Branch to isolate and test functionality

---
+++ APP/test.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+app = Flask(__name__)
+
+# DB = SQLAlchemy()
+# 
+# df = pd.read_csv('SpotifyAudioFeaturesApril2019.csv')
+#
+# conn = sqlite3.connect('songs_df.sqlite3')
+#
+# df.to_sql('songs', conn, if_exists='replace')
+
+# class Song(DB.Model):
+#     """ Model for song entry in database """
+#     id = DB.Column(DB.STring(30))
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs_df.sqlite3'
+#
+@app.route('/song/<int:track_id>', methods = ['POST'])
+def song(track_id):
+    """Route for recommendations based on song selected."""
+
+    #input
+    curs.execute()
+
+    #get parameters:
+    # use song_id
+    # songs_df = SELECT * from songs WHERE df_id == song_id
+    danceability = songs_df['danceability']
+    energy = songs_df['energy']
+
+
+    #model
+    model = "some pickled model"
+
+    #output
+    #should be 30 reccomendations
+    recommendations = model.predict("parameters")
+
+    return render_template('echo.html',song_name=song_name)
+#
+#
+# @app.route('/mood')
+# def mood():
+#     """Route foor recommendations based on mood selected."""
+#
+#     mood = request.get_json(force=True)
+#
+#     recommendations =
+#
+#
+# if __name__ == "__main__":
+#     app.run()
